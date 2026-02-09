259acad8cd00da34cd9ab44ea84a7b0c6900f73c
lpssss <lim55570@gmail.com>
2026-02-10 06:26:52 +0800

    Add script

---
+++ scripts/final_1_scripts/chap4/check_dataset_stats/myfyp_frompretrainedmodel_single_ucihar.bat
@@ -0,0 +1,35 @@
+@echo off
+
+REM Set environment variables
+set model_name=UniTS
+set d_model=128
+REM use script name as experiment name
+set exp_name=check_dataset_stats_myfyp_frompretrained_x128_supervised_single_ucihar_lr0.001
+set wandb_mode=disabled
+set ckpt_path=units_x128_pretrain_checkpoint.pth
+set random_port=1234
+set prj_name=%exp_name%_prj
+set task_data_config=data_provider/myfyp_single_ucihar.yaml
+
+REM Run the training script with torchrun
+python run.py ^
+  --is_training 1 ^
+  --model_id %exp_name% ^
+  --model %model_name% ^
+  --lradj supervised ^
+  --prompt_num 10 ^
+  --patch_len 16 ^
+  --stride 16 ^
+  --e_layers 3 ^
+  --d_model %d_model% ^
+  --des "train" ^
+  --learning_rate 0.001 ^
+  --weight_decay 5e-6 ^
+  --train_epochs 50 ^
+  --batch_size 64 ^
+  --acc_it 5 ^
+  --debug %wandb_mode% ^
+  --project_name %prj_name% ^
+  --clip_grad 100 ^
+  --pretrained_weight %ckpt_path% ^
+  --task_data_config_path %task_data_config%
