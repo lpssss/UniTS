57120b18bc9084596424c932da6399e4fd9803ec
lpssss <lim55570@gmail.com>
2026-03-08 14:42:04 +0800

    Add scripts to run inference quantized on raspberry pi

---
+++ scripts/final_1_scripts/chap5/quantized_inference_pi/8wdq/myfyp_frompretrainedmodel_prompttuning_single_wesad.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Set environment variables
+model_name=UniTS
+d_model=128
+base_name=myfyp_frompretrainedmodel_prompttuning_single_wesad_lr0.01_batch32
+quantization_type=8wdq
+exp_name=inference_tflite_pi_${base_name}_${quantization_type}
+wandb_mode=disabled
+ckpt_path=training_checkpoints/ALL_task_myfyp_frompretrainedmodel_prompttuning_single_wesad_lr0.01_batch32_UniTS_All_ftM_dm128_el3_train_0/ptune_checkpoint.pth
+random_port=1234
+prj_name=${exp_name}_prj
+task_data_config=data_provider/myfyp_single_wesad.yaml
+calib_data_path=calib_data/save_calib_${base_name}_calib_data.npz
+tflite_path=final_tflite_models/conversion_${base_name}_quantized_${quantization_type}.tflite
+
+# Run the training script with torchrun
+python run.py \
+  --is_training 0 \
+  --tflite_path "$tflite_path" \
+  --model_id ${exp_name} \
+  --model ${model_name} \
+  --lradj prompt_tuning \
+  --prompt_num 10 \
+  --patch_len 16 \
+  --stride 16 \
+  --e_layers 3 \
+  --d_model ${d_model} \
+  --des "train" \
+  --itr 1 \
+  --learning_rate 0.01 \
+  --weight_decay 0 \
+  --train_epochs 0 \
+  --prompt_tune_epoch 20 \
+  --batch_size 32 \
+  --acc_it 5 \
+  --debug ${wandb_mode} \
+  --project_name ${prj_name} \
+  --clip_grad 100 \
+  --pretrained_weight ${ckpt_path} \
+  --task_data_config_path ${task_data_config}
---
+++ scripts/final_1_scripts/chap5/quantized_inference_pi/run_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+bash scripts/final_1_scripts/chap5/quantized_inference_pi/8wdq/inference_tflite_all.sh
+bash scripts/final_1_scripts/chap5/quantized_inference_pi/8w8a/inference_tflite_all.sh
+bash scripts/final_1_scripts/chap5/quantized_inference_pi/fp/inference_tflite_all.sh
+bash scripts/final_1_scripts/chap5/quantized_inference_pi/8w16a/inference_tflite_all.sh
